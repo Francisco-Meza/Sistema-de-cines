62458a9aeaa64758a240cf1cad6a46888dbd48cc
Francisco-Meza <L201140051@surnayarit.tecnm.mx>
2022-12-08 10:34:07 -0700

    Acomode todo el pedo

---
+++ DAL/ClsConexion.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class ClsConexion
+    {
+        private string _base;
+        private string _servidor;
+        private string _usuario;
+        private string _clave;
+        private bool _seguridad;
+        private static ClsConexion con = null;
+
+        private ClsConexion()
+        {
+            this._base = "CINEMAT_IXTLAN";
+            this._servidor = Constantes.francisco2;
+            this._usuario = "sa";
+            this._clave = "021202";
+            this._seguridad = true;
+        }
+
+        public SqlConnection CreateConnection()
+        {
+            SqlConnection cadena = new SqlConnection();
+            try
+            {
+                cadena.ConnectionString = "Server=" + this._servidor + "; Database=" + this._base + ";";
+                if (_seguridad)
+                {
+                    cadena.ConnectionString += "Integrated Security = SSPI;";
+                }
+                else
+                {
+                    cadena.ConnectionString += "User Id =" + this._usuario + "; Password=" + this._clave + ";";
+                }
+                return cadena;
+            }
+            catch (Exception e)
+            {
+                return null;
+                throw e;
+            }
+        }
+
+        public static ClsConexion GetInstancia()
+        {
+            if (con == null)
+            {
+                con = new ClsConexion();
+            }
+            return con;
+        }
+    }
+}
---
+++ DAL/Constantes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public static class Constantes
+    {
+        public const string francisco = "DESKTOP-4MR4OFL\\MSSQLSERVER00";
+        public const string francisco2 = "DESKTOP-UJNFMIH";
+        public const string marlene = "DESKTOP-D8E6BU1";
+        public const string david = "Data Source=LAPTOP-MOLTPACP\\SQLEXPRESS;Initial Catalog=Restaurante;User ID=Master;Password=MAFRAD2;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public const string danny = "Data Source=DANNY;Initial Catalog = Restaurante; User ID = Master; Password=********;Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=Read";
+    }
+}
